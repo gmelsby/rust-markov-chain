3aec13128d9fee9b06f8dd048d64f6770752729c
Gregory Melsby <greg27426@gmail.com>
2024-08-20 17:05:43 -0400

    optionally specify ngram length as CLI arg

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,79 @@
+use rand::seq::SliceRandom;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
-use rand::seq::SliceRandom;
 
-fn read_lines<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>> 
-where P: AsRef<Path>, {
+fn read_lines<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
     // returns Err if this open fails
     let file = File::open(file_path)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-fn insert_into_hash_map(dict: &mut HashMap<Vec<String>, Vec<String>>, prior_ngram: Vec<String>, current: String) {
+fn insert_into_hash_map(
+    dict: &mut HashMap<Vec<String>, Vec<String>>,
+    prior_ngram: Vec<String>,
+    current: String,
+) {
     match dict.entry(prior_ngram) {
-        Entry::Vacant(e) => { e.insert(vec![current]); },
-        Entry::Occupied(mut e) => { e.get_mut().push(current); }
+        Entry::Vacant(e) => {
+            e.insert(vec![current]);
+        }
+        Entry::Occupied(mut e) => {
+            e.get_mut().push(current);
+        }
     }
 }
 
 // returns a function that adds a space to the front of a token if it is not one of the characters
 fn format_token_creator(characters: String) -> impl Fn(&String) -> String {
-    move |tk| format!("{}{}",  if !characters.contains(&*tk) {" "} else {""}, *tk)
+    move |tk| {
+        format!(
+            "{}{}",
+            if !characters.contains(&*tk) { " " } else { "" },
+            *tk
+        )
+    }
 }
 
-fn get_random_n_gram(dict: &HashMap<Vec<String>, Vec<String>>) -> Option<Vec<String>>
-{
+fn get_random_n_gram(dict: &HashMap<Vec<String>, Vec<String>>) -> Option<Vec<String>> {
     // make binding for borrow checker
-    let binding = dict
-        .keys()
-        .cloned()
-        .collect::<Vec<Vec<String>>>();
+    let binding = dict.keys().cloned().collect::<Vec<Vec<String>>>();
 
     let random_n_gram = binding.choose(&mut rand::thread_rng());
     random_n_gram.cloned()
 }
 
 fn print_n_gram(n_gram: &Vec<String>, format_token: &impl Fn(&String) -> String) {
-        for token in n_gram {
-            print!("{}", format_token(&token));
-        }
+    for token in n_gram {
+        print!("{}", format_token(&token));
+    }
 }
 
 fn push_to_prior_tokens(prior_tokens: &mut Vec<String>, token: String) {
     let prior_tokens_length = prior_tokens.len();
-        for i in 0..(prior_tokens_length - 1) {
-        prior_tokens[i] = prior_tokens[i+1].clone();
+    for i in 0..(prior_tokens_length - 1) {
+        prior_tokens[i] = prior_tokens[i + 1].clone();
     }
     prior_tokens[prior_tokens_length - 1] = token;
 }
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    let n_gram_length = 3;
+    // if we have a 2nd CLI argument use that for n-gram length, defaults to 3
+    let n_gram_length = args
+        .get(2)
+        .and_then(|arg| arg.parse().ok())
+        .map(|n| if (2..=4).contains(&n) { n } else { 3 })
+        .unwrap_or(3);
 
-    let mut ngram_dict: HashMap<Vec<String>, Vec<String>> =  HashMap::new();
+    let mut ngram_dict: HashMap<Vec<String>, Vec<String>> = HashMap::new();
 
     // string of single-character tokens that we want to separate from the end of words
     // we do not want to add a space before these tokens when printing
@@ -74,26 +90,43 @@ fn main() {
             if line.len() == 0 {
                 insert_into_hash_map(&mut ngram_dict, prior_tokens.clone(), "\n".to_string());
                 push_to_prior_tokens(&mut prior_tokens, "\n".to_string());
-           } else {
+            } else {
                 for word in line.split_whitespace() {
-                    // check if word is more than one character and end character needs to be split 
-                    if word.len() > 1 && word.ends_with(|c| {
-                        for symbol in no_space_tokens.chars() {
-                            if c == symbol {
-                                return true;
+                    // check if word is more than one character and end character needs to be split
+                    if word.len() > 1
+                        && word.ends_with(|c| {
+                            for symbol in no_space_tokens.chars() {
+                                if c == symbol {
+                                    return true;
+                                }
                             }
-                        }
-                        false
-                    }) {
-                        let word_body = word[0..word.len()-1].to_string();
-                        insert_into_hash_map(&mut ngram_dict, prior_tokens.clone(), word_body.clone());
+                            false
+                        })
+                    {
+                        let word_body = word[0..word.len() - 1].to_string();
+                        insert_into_hash_map(
+                            &mut ngram_dict,
+                            prior_tokens.clone(),
+                            word_body.clone(),
+                        );
                         push_to_prior_tokens(&mut prior_tokens, word_body.clone());
 
-                        let word_ending = word[word.len()-1..word.len()].to_string();
-                        insert_into_hash_map(&mut ngram_dict, prior_tokens.clone(), word_ending.clone());
-                        push_to_prior_tokens(&mut prior_tokens, word[word.len()-1..word.len()].to_string());
+                        let word_ending = word[word.len() - 1..word.len()].to_string();
+                        insert_into_hash_map(
+                            &mut ngram_dict,
+                            prior_tokens.clone(),
+                            word_ending.clone(),
+                        );
+                        push_to_prior_tokens(
+                            &mut prior_tokens,
+                            word[word.len() - 1..word.len()].to_string(),
+                        );
                     } else {
-                        insert_into_hash_map(&mut ngram_dict, prior_tokens.clone(), word.to_string());
+                        insert_into_hash_map(
+                            &mut ngram_dict,
+                            prior_tokens.clone(),
+                            word.to_string(),
+                        );
                         push_to_prior_tokens(&mut prior_tokens, word.to_string());
                     }
                 }
@@ -114,9 +147,8 @@ fn main() {
             prior_tokens = get_random_n_gram(&ngram_dict).unwrap();
             print_n_gram(&prior_tokens, &format_token)
         }
-        
-        
-        // choose next token from 
+
+        // choose next token from
         let mut next_token = "\n".to_string();
         let next_token_candidate = next_token_list.unwrap().choose(&mut rand::thread_rng());
         match next_token_candidate {
