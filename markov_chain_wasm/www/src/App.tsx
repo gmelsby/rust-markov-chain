b3131e9585e0f25298082a09edcc2ab9b6301de8
Gregory Melsby <greg@gregexp.com>
2025-01-31 13:18:16 -0500

    basic chain selection and ngram length able to be selected

--- markov_chain_wasm/www/src/App.tsx
+++ markov_chain_wasm/www/src/App.tsx
@@ -4,24 +4,40 @@ import './App.css'
 
 function App() {
   const [markovChain, setMarkovChain] = useState<WasmMarkovChain | null>(null);
+  const [chainList, setChainList] = useState<string[]>([]);
+  const [selectedChains, setSelectedChains] = useState<{ name: string, weight: number }[]>([]);
+  const [ngramLength, setNgramLength] = useState(2);
   const [output, setOutput] = useState<string[]>([]);
   const [generating, setGenerating] = useState(false);
 
   useEffect(() => {
     const initializeChain = async () => {
-      const chain = new WasmMarkovChain(2); // Example n-gram length
+      const chain = new WasmMarkovChain(ngramLength);
       setMarkovChain(chain);
     };
 
     initializeChain();
+  }, [ngramLength]);
+
+  useEffect(() => {
+    const fetchChains = async () => {
+      const chainResponse = await fetch('chains/');
+      const chainObjects = await chainResponse.json();
+      setChainList(chainObjects.map((o: { name: string }) => o.name))
+    }
+
+    fetchChains();
+
   }, []);
 
+
+
   const handleLoadChain = async () => {
-    if (markovChain) {
-      await markovChain.load_chain(`/chains/${import.meta.env.VITE_CHAIN_1}/2`, 1);
-      console.log("loaded chain 1");
-      await markovChain.load_chain(`/chains/${import.meta.env.VITE_CHAIN_2}/2`, 1);
-      console.log("loaded chain 2");
+    if (markovChain && selectedChains.length > 0) {
+      for (const chainObject of selectedChains) {
+        await markovChain.load_chain(`/chains/${chainObject.name}/${ngramLength}`, chainObject.weight);
+        console.log(`loaded chain ${chainObject.name}`);
+      }
       const sentenceList = markovChain.find_sentence_start();
       console.log(sentenceList);
 
@@ -50,7 +66,7 @@ function App() {
           return [...t, formattedToken];
         });
 
-        timeoutId = setTimeout(generateTokens, 100);
+        timeoutId = setTimeout(generateTokens, 70);
       }
 
     }
@@ -85,6 +101,12 @@ function App() {
   return (
     <>
       <h1>Markov Chain</h1>
+      <h2>{chainList.map(chain => <span onClick={() => setSelectedChains(chains => [...chains, { name: chain, weight: 1 }])} key={chain}>{chain} </span>)}</h2>
+      <h2>{selectedChains.map(c => c.name).join(" ")}</h2>
+      <select value={ngramLength} onChange={e => setNgramLength(Number(e.target.value))}>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
       <div>
         <button onClick={() => handleLoadChain()}>
           Click to Load
