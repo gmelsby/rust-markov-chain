d91ad1b94a7aef3e26dd746744a4b93510db460d
Gregory Melsby <greg@gregexp.com>
2025-01-31 22:19:11 -0500

    set up tailwindcss

--- markov_chain_wasm/www/vite.config.ts
+++ markov_chain_wasm/www/vite.config.ts
@@ -1,10 +1,15 @@
 import wasm from "vite-plugin-wasm"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [
+    react(),
+    wasm(),
+    tailwindcss()
+  ],
   server: {
     proxy: {
       '/chains/': {
