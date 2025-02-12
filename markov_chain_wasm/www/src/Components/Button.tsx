d76536779e7edb733d944c93986055e8e2e5f36e
Gregory Melsby <greg@gregexp.com>
2025-02-12 18:58:24 -0500

    holding backspace works for touch-enabled devices

--- markov_chain_wasm/www/src/Components/Button.tsx
+++ markov_chain_wasm/www/src/Components/Button.tsx
@@ -86,6 +86,7 @@ function Button({ onClick, onLongPress, longPressOptions, children, disabled, si
     if (longPressRepeatTimeout.current) {
       clearTimeout(longPressRepeatTimeout.current)
     }
+    setIsLongPress(false);
     setIsPress(false);
   }
 
@@ -100,6 +101,9 @@ function Button({ onClick, onLongPress, longPressOptions, children, disabled, si
       onMouseUp={disabled || !onLongPress ? undefined : handleMouseUp}
       onMouseLeave={disabled || !onLongPress ? undefined : handleMouseLeave}
       onClick={disabled || onLongPress ? undefined : onClick}
+      onTouchStart={disabled || !onLongPress ? undefined : handleMouseDown}
+      onTouchEnd={disabled || !onLongPress ? undefined : handleMouseUp}
+      onTouchCancel={disabled || !onLongPress ? undefined : handleMouseLeave}
     >
       {children}
     </button>
