b7499bceeedd497d3398a06102f6543b903b3f5f
Gregory Melsby <greg@gregexp.com>
2025-02-18 17:11:46 -0500

    fixed typo with cursor-pointer

--- markov_chain_wasm/www/src/Components/TopBar.tsx
+++ markov_chain_wasm/www/src/Components/TopBar.tsx
@@ -3,8 +3,8 @@ function TopBar() {
   return (
     <div className={`sticky top-0 w-full h-12 z-20 bg-neutral-950`}>
       <div className='flex flex-row items-center justify-between h-full'>
-        <div className='cursor-hover mx-2' onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}>
-          <h1>Markov Chain</h1>
+        <div className='cursor-pointer mx-3 lg:mx-4' onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })}>
+          <h1 className='font-medium'>Markov Chain</h1>
         </div>
       </div>
     </div >
