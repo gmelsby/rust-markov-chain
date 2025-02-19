b84364eb80cd8998865fe351e879833802f6f1c8
Gregory Melsby <greg@gregexp.com>
2025-02-19 17:13:43 -0500

    chains label now becomes horizontal on screen sizes up to sm

--- markov_chain_wasm/www/src/Components/ChainControlPanel.tsx
+++ markov_chain_wasm/www/src/Components/ChainControlPanel.tsx
@@ -101,15 +101,18 @@ function ChainControlPanel({
 
   return (
     <>
-      <div className="flex flex-wrap mb-0 p-5 border-b-4 border-r-2 border-l-2 border-neutral-700 rounded-b-2xl bg-neutral-800/20 justify-between">
-        <div className="flex grow border-2 border-neutral-500 rounded-2xl bg-neutral-700 m-4 min-w-0">
-          <div className="shrink-0 bg-neutral-950/90 flex flex-col my-3 justify-center rounded-br-full rounded-tr-full w-8">
-            <div className="[writing-mode:vertical-lr] rotate-180 flex items-center justify-center">
+      <div className="flex flex-wrap mb-0 p-5 justify-between">
+        <div className="flex grow flex-wrap sm:flex-nowrap border-2 border-neutral-500/60 rounded-2xl bg-neutral-700/85 m-4 min-w-0 justify-center">
+          <div
+            className="shrink-0 bg-neutral-950/90 flex flex-col mx:2 sm:mx-0 sm:my-3 justify-center
+            rounded-br-full sm:rounded-tr-full rounded-tr-none sm:rounded-bl-none rounded-bl-full w-[calc(100%-1rem)] sm:w-8"
+          >
+            <div className="sm:[writing-mode:vertical-lr] sm:rotate-180 flex items-center justify-center">
               <p className="text-neutral-300 font-bold">Chains</p>
             </div>
           </div>
 
-          <div className="flex flex-wrap shrink grow ml-2 min-w-0">
+          <div className="flex flex-wrap shrink grow mx-2 min-w-0">
             {selectedChains.map((c, i) => (
               <SelectedChainDisplay
                 chain={c}
@@ -131,23 +134,23 @@ function ChainControlPanel({
             />
           </div>
         </div>
-        <div className="flex">
-          <div className="flex flex-col items-center justify-around border-2 rounded-2xl border-neutral-500/50 bg-neutral-700/50 p-3 m-5 min-h-50 ">
-            <h3 className="font-bold">Controls</h3>
-            <div className="flex flex-row items-center m-2">
-              <div className="h-12 flex flex-row items-center px-3 rounded-tl-md rounded-bl-md bg-neutral-950 border-2 border-neutral-950">
-                <h3 className="font-medium">N-gram Length:</h3>
-              </div>
-              <select
-                className="mr-1 h-12 items-center justify-center rounded-br-md rounded-tr-md bg-neutral-950/70 px-3 border-2 border-neutral-950 font-bold text-neutral-50 hover:bg-blue-950 cursor-pointer"
-                value={ngramLength}
-                onChange={(e) => setNgramLength(Number(e.target.value))}
-              >
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
+        <div className="flex flex-col items-center justify-around border-2 rounded-2xl border-neutral-500/60 bg-neutral-700/85 p-3 m-4 min-h-50 grow sm:grow-0">
+          <h3 className="font-bold m-2">Controls</h3>
+          <div className="flex flex-row items-center m-2">
+            <div className="h-12 flex flex-row items-center px-3 rounded-tl-md rounded-bl-md bg-neutral-950 border-2 border-neutral-950">
+              <h3 className="font-medium">N-gram Length:</h3>
             </div>
+            <select
+              className="mr-1 h-12 items-center justify-center rounded-br-md rounded-tr-md bg-neutral-950/70 px-3 border-2 border-neutral-950 font-bold text-neutral-50 hover:bg-blue-950 cursor-pointer"
+              value={ngramLength}
+              onChange={(e) => setNgramLength(Number(e.target.value))}
+            >
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
 
+          <div className="m-2">
             <Button
               onClick={loaded ? resetChain : handleLoadChains}
               disabled={selectedChains.length === 0}
