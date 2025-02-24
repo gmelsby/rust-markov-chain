2403248bd872bb3b5beac1ad2dcfc78d731f2830
Gregory Melsby <greg@gregexp.com>
2025-02-24 15:22:51 -0500

    made IndededDB operations more streamlined

--- markov_chain_wasm/www/src/Models/IndexedDb.tsx
+++ markov_chain_wasm/www/src/Models/IndexedDb.tsx
@@ -1,44 +1,43 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const IDB_VERSION_NO = 1;
+const STORE_NAMES = ["2", "3"] as const;
 
 async function openAndUpgrade(chainVersion: string) {
   return await openDB(`chains/${chainVersion}`, IDB_VERSION_NO, {
     upgrade(db) {
       // Handle creating object stores if they don't exist
-      if (!db.objectStoreNames.contains('2')) {
-        db.createObjectStore('2');
-      }
-      if (!db.objectStoreNames.contains('3')) {
-        db.createObjectStore('3');
-      }
-    }
+      STORE_NAMES.forEach((n) => {
+        if (!db.objectStoreNames.contains(n)) {
+          db.createObjectStore(n);
+        }
+      });
+    },
   });
 }
 
 export async function readIdbChains(chainVersion: string) {
   const db = await openAndUpgrade(chainVersion);
-  const transaction = db.transaction('3', 'readonly');
-  const store = transaction.objectStore('3');
-  const result = (await store.getAllKeys()).map(c => c.toString());
+  const transaction = db.transaction("3", "readonly");
+  const store = transaction.objectStore("3");
+  const result = (await store.getAllKeys()).map((c) => c.toString());
   db.close();
   return result;
 }
 
 export async function deleteIdbChain(chainName: string, chainVersion: string) {
   const db = await openAndUpgrade(chainVersion);
-  const transaction = db.transaction(['2', '3'], 'readwrite');
-  transaction.objectStore('3').delete(chainName);
-  transaction.objectStore('2').delete(chainName);
-  await transaction.done
+  const transaction = db.transaction(STORE_NAMES, "readwrite");
+  await Promise.all(
+    STORE_NAMES.map((n) => transaction.objectStore(n).delete(chainName)),
+  );
   db.close();
 }
 
 export async function deleteAllIdbChains(chainVersion: string) {
   const db = await openAndUpgrade(chainVersion);
-  const transaction = db.transaction(['2', '3'], 'readwrite');
-  transaction.objectStore('3').clear();
-  transaction.objectStore('2').clear();
-  await transaction.done;
+
+  const transaction = db.transaction(STORE_NAMES, "readwrite");
+  await Promise.all(STORE_NAMES.map((n) => transaction.objectStore(n).clear()));
   db.close();
 }
