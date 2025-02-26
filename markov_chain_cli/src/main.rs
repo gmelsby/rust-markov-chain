16ec77173b505ae8ae2f3264fbd56000e15d6898
Gregory Melsby <greg@gregexp.com>
2025-02-26 16:43:43 -0500

    added capacity to modify probability distribution with exponents

--- markov_chain_cli/src/main.rs
+++ markov_chain_cli/src/main.rs
@@ -113,7 +113,7 @@ fn run(args: &Vec<String>) -> Result<(), std::io::Error> {
 
     println!("Chain loaded... generating output\n");
     for _ in 0..1000 {
-        let next_tokens = markov_chain.peek_next_tokens(1).clone();
+        let next_tokens = markov_chain.peek_next_tokens(1, 1.0).clone();
 
         match markov_chain.put_next_token(next_tokens[0].1) {
             Ok((tk, _)) => {
