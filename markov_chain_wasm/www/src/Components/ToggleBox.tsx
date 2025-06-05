253c7bb25bec7b6299e180352d8dd87add400154
Gregory Melsby <greg@gregexp.com>
2025-06-04 21:17:28 -0400

    styled token toggles

--- markov_chain_wasm/www/src/Components/ToggleBox.tsx
+++ markov_chain_wasm/www/src/Components/ToggleBox.tsx
@@ -3,9 +3,16 @@ function ToggleBox({ label, isChecked, setIsChecked }: { label: string, isChecke
     setIsChecked(e.target.checked)
   };
   return (
-    <label className='inline-flex items-center cursor-pointer'>
-      <input type='checkbox' checked={isChecked} onChange={handleCheckboxChange} />
-      <span className='text-sm font-medium'>{label}</span>
+    // Credit to https://flowbite.com/docs/components/toggle/ for majority of toggle styling
+    <label className='inline-flex items-center cursor-pointer m-1'>
+      <input type='checkbox' checked={isChecked} value="" className="sr-only peer" onChange={handleCheckboxChange} />
+      <div className="relative w-11 h-6 bg-neutral-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 
+      dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-neutral-700 peer-checked:after:translate-x-full 
+      rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] 
+      after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border 
+      after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-neutral-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600">
+      </div>
+      <span className='ms-3 text-sm font-medium'>{label}</span>
     </label>
   );
 }
--- markov_chain_wasm/www/src/Components/ToggleTokens.tsx
+++ markov_chain_wasm/www/src/Components/ToggleTokens.tsx
@@ -22,12 +22,14 @@ function ToggleTokens() {
   return (
     <div className='flex flex-col items-center m-3'>
       <h3 className='font-semibold text-lg my-3'>Toggle Special Token Display in Output</h3>
-      <ToggleBox label='Hide Parentheses' isChecked={hideParentheses} setIsChecked={setHideParentheses} />
-      <ToggleBox label='Hide Square Brackets' isChecked={hideSquareBrackets} setIsChecked={setHideSquareBrackets} />
-      <ToggleBox label='Hide Double Quotes' isChecked={hideDoubleQuotes} setIsChecked={setHideDoubleQuotes} />
-      <ToggleBox label='Hide Single Quotes' isChecked={hideSingleQuotes} setIsChecked={setHideSingleQuotes} />
-      <ToggleBox label='Hide Underscores' isChecked={hideUnderscores} setIsChecked={setHideUnderscores} />
-      <ToggleBox label='Hide Start/End Token' isChecked={hideStartEnd} setIsChecked={setHideStartEnd} />
+      <div className='flex flex-col'>
+        <ToggleBox label='Hide Parentheses' isChecked={hideParentheses} setIsChecked={setHideParentheses} />
+        <ToggleBox label='Hide Square Brackets' isChecked={hideSquareBrackets} setIsChecked={setHideSquareBrackets} />
+        <ToggleBox label='Hide Double Quotes' isChecked={hideDoubleQuotes} setIsChecked={setHideDoubleQuotes} />
+        <ToggleBox label='Hide Single Quotes' isChecked={hideSingleQuotes} setIsChecked={setHideSingleQuotes} />
+        <ToggleBox label='Hide Underscores' isChecked={hideUnderscores} setIsChecked={setHideUnderscores} />
+        <ToggleBox label='Hide Start/End Token' isChecked={hideStartEnd} setIsChecked={setHideStartEnd} />
+      </div>
     </div>
   );
 }
